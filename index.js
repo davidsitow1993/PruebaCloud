a1cf8dc2ded0e8b144b4f5a302066e9aecd23b60
davidsitow1993 <david-alex131@hotmail.com>
2017-11-13 12:15:19 -0500

    cambio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Prueba Cloud David Pillajo, Andres Salgado')
 })
 
 app.listen(app.get('port'), function() {
